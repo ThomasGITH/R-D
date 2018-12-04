1f703b3697db1b533916083cf77f5129c7998590
Thomas Linssen <31830553+ThomasGITH@users.noreply.github.com>
2018-12-04 17:45:40 +0100

    Add files via upload

--- JSON opdracht 2/schools.php
+++ JSON opdracht 2/schools.php
@@ -6,18 +6,23 @@ if(file_exists($file)){
     $objson = json_decode($filedata, true);
 
     $school = $_POST["school"];
-    $category = $_POST["category"];
-    $nr_students = $_POST["nr_students"];
-    $course1 = $_POST["course1"];
-    $course2 = $_POST["course2"];
 
-    $objson["school"][$school]["category"] = $category;
-    $objson["school"][$school]["nr_students"] = $nr_students;
-    $objson["school"][$school]["courses"]["Course1"] = $course1;
-    $objson["school"][$school]["courses"]["Course2"] = $course2;
+    $objson["school"][$school]["category"] = $_POST["category"];
+    $objson["school"][$school]["nr_students"] = $_POST["nr_students"];
+    $objson["school"][$school]["courses"]["Course1"] = $_POST["course1"];
+    $objson["school"][$school]["courses"]["Course2"] = $_POST["course2"];
+
+    writeJson($objson, "my_file.json");
 
     echo"<hr><code><pre>";
     print_r($objson);
     echo"</pre></code></hr>";
 }
 else echo $file .' not exists';
+
+function writeJson($objson, $fileOutput){
+    $saveJson = json_encode($objson);
+    $file = fopen($fileOutput, "w") or die ("can't open file");
+    fwrite($file, $saveJson);
+    fclose($file);
+}
